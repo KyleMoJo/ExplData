4d017e1b838ea8245638f731ee68f277e9ea7714
KyleMoJo <kmasonjones@yahoo.com>
2015-08-04 20:35:59 +0200

    First file with working data extraction

---
+++ DraftPlot1.R
@@ -0,0 +1,8 @@
+fullelec <- read.table("household_power_consumption.txt", header = TRUE, 
+                       sep = ";", comment.char = "", na.strings = "?", 
+                       colClasses = c("character", "character", rep("numeric",7)))
+Day1 <- fullelec[,1] == "1/2/2007"
+Day2 <- fullelec[,1] == "2/2/2007"
+Want <- as.logical(Day1 + Day2)
+elec <- fullelec[Want,]
+rm(list = c("fullelec","Day1","Day2", "Want"))
