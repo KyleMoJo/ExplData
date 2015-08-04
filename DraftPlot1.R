aac388e1c3f9eabbc51615d82afd12639bd9d50f
KyleMoJo <kmasonjones@yahoo.com>
2015-08-04 23:41:05 +0200

    added extraction of desired days and date&time conversion to POSIXct

--- DraftPlot1.R
+++ DraftPlot1.R
@@ -5,4 +5,10 @@ Day1 <- fullelec[,1] == "1/2/2007"
 Day2 <- fullelec[,1] == "2/2/2007"
 Want <- as.logical(Day1 + Day2)
 elec <- fullelec[Want,]
-rm(list = c("fullelec","Day1","Day2", "Want"))
+timelt <- lapply(paste(elec[,1],elec[,2]), strptime, format = "%d/%m/%Y %H:%M:%S")
+timect <- sapply(timelt, as.POSIXct)
+class(timect) <- "POSIXct"
+elec <- cbind(timect,elec[,3:9])
+names(elec)[1] = "Date&Time"
+rm(list = c("fullelec","Day1","Day2", "Want", "timelt", "timect"))
+
